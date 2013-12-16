24df27213e3c32a0f940209cb3308c24cf8b1cae
Sharif Olorin <sio@tesser.org>
2013-12-16 06:11:14 +0000

    Comment some of the things

--- src/frame.h
+++ src/frame.h
@@ -1,12 +1,18 @@
 #include "DataFrame.pb-c.h"
 #include "anchor_stats.h"
 
+/* Worker function called by the type-specific as_send_* functions in
+ * anchor_stats.c. */
 int as_send_frame( as_connection connection
               , DataFrame *frame);
 
+/* Create a tag object from a field and a value. */
 DataFrame__Tag* build_frame_tag(char* field, char* value);
 
-DataFrame* build_frame( char **tag_fields
+/* Create a DataFrame object with everything set except the value itself
+ * (the value is set by the type-specific functions that call
+ * build_frame_skel). */
+DataFrame* build_frame_skel( char **tag_fields
                       , char **tag_values
                       , int tag_count
                       , uint64_t timestamp
