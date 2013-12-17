78ed45d205bef4073b3b472df6ce3ad1b230e829
Christian Marie <pingu@anchor.net.au>
2013-12-17 16:33:31 +1100

    Spaces are pretty

--- src/macros.h
+++ src/macros.h
@@ -1,8 +1,8 @@
 #include <syslog.h>
-#define fail_if( assertion, action, ... ) do {                           \
-        if ( assertion ){                                                \
-                syslog( LOG_ERR, "libanchor_stats error:" __VA_ARGS__ ); \
-                { action };                                              \
+#define fail_if( assertion, action, ... ) do {                            \
+        if ( assertion ){                                                 \
+                syslog( LOG_ERR, "libanchor_stats error: " __VA_ARGS__ ); \
+                { action };                                               \
         } } while( 0 )
 
 
