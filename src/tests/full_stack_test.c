7c0369eb4616e25ff44c9e14016c00dbf94ede9f
Christian Marie <pingu@anchor.net.au>
2013-12-23 10:52:52 +1100

    Add many_messages test

--- src/tests/full_stack_test.c
+++ src/tests/full_stack_test.c
@@ -18,6 +18,8 @@ void setup( fixture *f, gconstpointer td ){
         g_assert( f->connection );
 }
 void teardown( fixture *f, gconstpointer td ){
+        as_close( f->connection );
+        as_consumer_shutdown( f->context );
 }
 
 // Starting simple, we send one message and make sure we get it.
@@ -25,9 +27,6 @@ void one_message( fixture *f, gconstpointer td ){
         char *field_buf[] = {"foo"};
         char *value_buf[] = {"bar"};
 
-        // Send a few messages
-        g_assert( as_send_int( f->connection, field_buf, value_buf, 1, 10, 20 )
-                  != -1 );
         g_assert( as_send_int( f->connection, field_buf, value_buf, 1, 10, 20 )
                   != -1 );
 
@@ -40,14 +39,45 @@ void one_message( fixture *f, gconstpointer td ){
 
         char *scratch = malloc(512);
         int recieved = zmq_recv( bind_sock, scratch, 512, 0 );
-        g_assert_cmpint( recieved, ==, 54 );
+        g_assert_cmpint( recieved, ==, 27 );
         free( scratch );
 
         zmq_close( bind_sock );
         zmq_ctx_destroy( context );
-        as_close( f->connection );
-        as_consumer_shutdown( f->context );
 }
+
+// Now send a bunch, we should get them all in one data-burst.
+void many_messages( fixture *f, gconstpointer td ){
+        // This time we start up the server first, to ensure there's no
+        // difference there.
+        void *context = zmq_ctx_new();
+        g_assert( context );
+        void *bind_sock = zmq_socket( context, ZMQ_PULL );
+        g_assert( bind_sock );
+        g_assert( !zmq_bind( bind_sock, "ipc:///tmp/as_full_stack_test" ) );
+
+
+        // Send a few messages
+        int i;
+        char *field_buf[] = {"foo"};
+        char *value_buf[] = {"bar"};
+        for( i = 0; i < 8192; i++ )
+                g_assert( as_send_int( f->connection
+                                     , field_buf
+                                     , value_buf
+                                     , 1
+                                     , 10
+                                     , 20 ) != -1 );
+
+        char *scratch = malloc(512);
+        int recieved = zmq_recv( bind_sock, scratch, 512, 0 );
+        g_assert_cmpint( recieved, ==, 221184 );
+        free( scratch );
+
+        zmq_close( bind_sock );
+        zmq_ctx_destroy( context );
+}
+
 
 // The aim here is to test a bunch of messages being sent without an upstream
 // connection, so many that we are forced to defer to disk to save memory.
@@ -64,6 +94,12 @@ int main( int argc, char **argv ){
                   , NULL
                   , setup
                   , one_message
+                  , teardown );
+        g_test_add( "/full_stack/many_messages"
+                  , fixture
+                  , NULL
+                  , setup
+                  , many_messages
                   , teardown );
         return g_test_run();
 }
