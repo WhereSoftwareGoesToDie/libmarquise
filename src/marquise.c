624353a1e140be83bd293c74be3fd41e5477ff16
Sharif Olorin <sio@tesser.org>
2014-06-05 14:37:35 +1000

    Validate namespace

---
+++ src/marquise.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+/* Return 1 if namespace is valid (only alphanumeric characters), otherwise
+ * return 0. */
+uint8_t valid_namespace(char **namespace) {
+	size_t len = strlen(namespace);
+	int i;
+	for (i = 0; i < len; i++) {
+		if (
+			(namespace[i] | 32 < 'a' || namespace[i] | 32 > 'z') && 
+			(namespace[i] < '0' || namespace[i] > '9')
+		) {
+			return 0;
+	}
+	return 1;
+}
