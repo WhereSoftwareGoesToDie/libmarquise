8d507d3b9ab859bd4375ffb617357b34c84baf4b
Sharif Olorin <sio@tesser.org>
2014-06-19 14:22:05 +1000

    Hex, binary, same thing, right?

--- src/marquise.c
+++ src/marquise.c
@@ -89,7 +89,7 @@ int marquise_send_simple(marquise_ctx *ctx, uint64_t address, uint64_t timestamp
 	}
 	uint8_t buf[24];
 	/* Set the LSB for a simple frame. */
-	address &= 0x1111111111111111 - 1;
+	address &= 0xffffffffffffffff-1;
 	U64TO8_LE(buf, address);
 	U64TO8_LE(buf+8, timestamp);
 	U64TO8_LE(buf+16, value);
@@ -108,7 +108,7 @@ int marquise_send_extended(marquise_ctx *ctx, uint64_t address, uint64_t timesta
 	uint8_t *buf = malloc(buf_len);
 	uint64_t length_word = value_len;
 	/* Set the LSB for an extended frame. */
-	address |= 0x0000000000000001; 
+	address |= 1; 
 	U64TO8_LE(buf, address);
 	U64TO8_LE(buf+8, timestamp);
 	U64TO8_LE(buf+16, length_word);
