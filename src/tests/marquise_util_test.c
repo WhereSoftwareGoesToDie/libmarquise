b020472a9ad91dd6a7d950aeeb49af36170cd334
Sharif Olorin <sio@tesser.org>
2014-06-20 17:10:16 +1000

    Add test for spool path construction
    Signed-off-by: Sharif Olorin <sio@tesser.org>

--- src/tests/marquise_util_test.c
+++ src/tests/marquise_util_test.c
@@ -18,9 +18,23 @@ void test_invalid_namespace() {
 	g_assert_cmpint(ret, ==, 0);
 }
 
+void test_build_spool_path() {
+	char *spool_path = build_spool_path("/tmp", "marquisetest");
+	char *expected_path = "/tmp/marquisetest/";
+	size_t expected_len = strlen(expected_path);
+	int i;
+	for (i=0; i < expected_len; i++) {
+		if (expected_path[i] != spool_path[i]) {
+			printf("Got path %s, expected path with prefix %s\n", spool_path, expected_path);
+			g_test_fail();
+		}
+	}
+}
+
 int main(int argc, char **argv) {
 	g_test_init(&argc, &argv, NULL);
 	g_test_add_func("/valid_namespace/valid", test_valid_namespace);
 	g_test_add_func("/valid_namespace/invalid", test_invalid_namespace);
+	g_test_add_func("/build_spool_path/path", test_build_spool_path);
 	return g_test_run();
 }
