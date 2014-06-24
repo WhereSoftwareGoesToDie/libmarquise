a2c2ed7a8008c1e6df2609eb298e6f16cfb2c22c
Sharif Olorin <sio@tesser.org>
2014-06-24 13:45:45 +1000

    Add test for marquise_send_simple
    Signed-off-by: Sharif Olorin <sio@tesser.org>

---
+++ src/tests/marquise_send_test.c
@@ -0,0 +1,24 @@
+#include <glib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#include "../marquise.h"
+
+void test_send_simple() {
+	setenv("MARQUISE_SPOOL_DIR", "/tmp", 1);
+	mkdir("/tmp/marquisetest", 0700);
+	marquise_ctx *ctx = marquise_init("marquisetest");
+	if (ctx == NULL) {
+		printf("marquise_init failed: %s\n", strerror(errno));
+		g_test_fail();
+	}
+	marquise_send_simple(ctx, 5, 100, 200000);
+}
+
+int main(int argc, char **argv) {
+	g_test_init(&argc, &argv, NULL);
+	g_test_add_func("/marquise_send/send_simple", test_send_simple);
+	return g_test_run();
+}
---
+++ src/tests/marquise_shutdown_test.c
@@ -0,0 +1,29 @@
+#include <glib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#include "../marquise.h"
+
+void test_shutdown() {
+	setenv("MARQUISE_SPOOL_DIR", "/tmp", 1);
+	marquise_ctx *ctx = marquise_init("marquisetest");
+	if (ctx == NULL) {
+		printf("marquise_init failed: %s\n", strerror(errno));
+		g_test_fail();
+		return;
+	}
+	int ret = marquise_shutdown(ctx);
+	if (ret != 0) {
+		printf("marquise_shutdown failed: %s\n", strerror(errno));
+		g_test_fail();	
+		return;
+	}
+}
+
+int main(int argc, char **argv) {
+	g_test_init(&argc, &argv, NULL);
+	g_test_add_func("/marquise_shutdown/shutdown", test_shutdown);
+	return g_test_run();
+}
