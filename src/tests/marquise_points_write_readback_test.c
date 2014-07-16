e81ae3a76540c4e5170a01113b6a7c222089c401
Barney Desmond <barney.desmond@anchor.net.au>
2014-07-16 18:54:01 +1000

    Add a readback consistency test for writing simple and extended datapoints

--- src/tests/marquise_points_write_readback_test.c
+++ src/tests/marquise_points_write_readback_test.c
@@ -6,6 +6,16 @@
 
 #include "../marquise.h"
 
+/* Be careful with the addresses we write, the LSB will be cleared for
+   simple points and set for extended points. */
+#define SIMPLE_ADDRESS   1234567890123456780
+#define SIMPLE_TIMESTAMP 1405392588998566144
+#define SIMPLE_VALUE     133713371337
+#define EXTENDED_ADDRESS   1234567890999999999
+#define EXTENDED_TIMESTAMP 1405392588999999999
+#define EXTENDED_VALUE     "hello world"
+#define EXTENDED_VALUE_LEN 11  // yes this is evil/dangerous, this is measured in bytes, which will be non-obvious if you have a non-ASCII UTF-8 test string
+
 /* Read 64 bits from little-endian byte array p, make a 64-bit value. */
 #define LE8TOU64(v, p) v = \
 	(((uint64_t)p[0])      ) + \
@@ -19,9 +29,6 @@
 
 
 void test_points_write_readback() {
-	/* Be careful with the addresses we write, the LSB will be cleared for
-	   simple points and set for extended points. */
-
 	int ret;
 
 	// Init
@@ -34,7 +41,7 @@ void test_points_write_readback() {
 	}
 
 	// Write simple (address/timestamp/value)
-	ret = marquise_send_simple(ctx, 1234567890123456780, 1405392588998566144, 133713371337);
+	ret = marquise_send_simple(ctx, SIMPLE_ADDRESS, SIMPLE_TIMESTAMP, SIMPLE_VALUE);
 	if (ret != 0) {
 		printf("marquise_send_simple failed: %s\n", strerror(errno));
 		g_test_fail();
@@ -42,7 +49,7 @@ void test_points_write_readback() {
 	}
 
 	// Write extended (address/timestamp/value/length)
-	ret = marquise_send_extended(ctx, 1234567890999999999, 1405392588999999999, "hello world", 11);
+	ret = marquise_send_extended(ctx, EXTENDED_ADDRESS, EXTENDED_TIMESTAMP, EXTENDED_VALUE, EXTENDED_VALUE_LEN);
 	if (ret != 0) {
 		printf("marquise_send_extended failed: %s\n", strerror(errno));
 		g_test_fail();
@@ -66,12 +73,16 @@ void test_points_write_readback() {
 		g_test_fail();
 		return;
 	}
-	// XXX: can we free(written_spool_path) now? Probably, once the file is open.
+	free(written_spool_path);
 
 	uint64_t address;
 	uint64_t timestamp;
 	uint64_t value;
+	char*    extended_value;
+	uint64_t extended_value_len;
 	unsigned char simple_buf[24];
+	unsigned char extended_header_buf[24];
+
 
 	// Read back a simple data point
 	ret = fread(simple_buf, 1, 24, spool);
@@ -82,6 +93,7 @@ void test_points_write_readback() {
 		if (ferror(spool) != 0) {
 			printf("Unexpected short read of %d bytes from spool while reading back simple data point, there was an error\n", ret);
 		}
+		fclose(spool);
 		printf("Can't continue after short read\n");
 		g_test_fail();
 		return;
@@ -90,35 +102,98 @@ void test_points_write_readback() {
 	LE8TOU64(timestamp, (simple_buf+8));
 	LE8TOU64(value,     (simple_buf+16));
 
-	if (address != 1234567890123456780) {
-		free(written_spool_path);
+	if (address != SIMPLE_ADDRESS) {
 		fclose(spool);
-		printf("Returned value of 'address' %lu doesn't match expected values of %lu\n", address, 1234567890123456780);
+		printf("Returned value of simple 'address' %lu doesn't match expected value of %lu\n", address, SIMPLE_ADDRESS);
 		g_test_fail();
 		return;
 	}
 
-	if (timestamp != 1405392588998566144) {
-		free(written_spool_path);
+	if (timestamp != SIMPLE_TIMESTAMP) {
 		fclose(spool);
-		printf("Returned value of 'timestamp' %lu doesn't match expected values of %lu\n", timestamp, 1405392588998566144);
+		printf("Returned value of simple 'timestamp' %lu doesn't match expected value of %lu\n", timestamp, SIMPLE_TIMESTAMP);
 		g_test_fail();
 		return;
 	}
 
-	if (value != 133713371337) {
-		free(written_spool_path);
+	if (value != SIMPLE_VALUE) {
 		fclose(spool);
-		printf("Returned value of 'value' %lu doesn't match expected values of %lu\n", value, 133713371337);
+		printf("Returned value of simple 'value' %lu doesn't match expected value of %lu\n", value, SIMPLE_VALUE);
 		g_test_fail();
 		return;
 	}
+
 
 	// Read back extended
-	// XXX: To be written
-	
+	ret = fread(extended_header_buf, 1, 24, spool);
+	if (ret != 24) {
+		if (feof(spool) != 0) {
+			printf("Unexpected short read of %d bytes from spool while reading back extended data point, end of file\n", ret);
+		}
+		if (ferror(spool) != 0) {
+			printf("Unexpected short read of %d bytes from spool while reading back extended data point, there was an error\n", ret);
+		}
+		fclose(spool);
+		printf("Can't continue after short read\n");
+		g_test_fail();
+		return;
+	}
+	LE8TOU64(address,             extended_header_buf);
+	LE8TOU64(timestamp,          (extended_header_buf+8));
+	LE8TOU64(extended_value_len, (extended_header_buf+16));
+
+	if (address != EXTENDED_ADDRESS) {
+		fclose(spool);
+		printf("Returned value of extended 'address' %lu doesn't match expected value of %lu\n", address, EXTENDED_ADDRESS);
+		g_test_fail();
+		return;
+	}
+
+	if (timestamp != EXTENDED_TIMESTAMP) {
+		fclose(spool);
+		printf("Returned value of extended 'timestamp' %lu doesn't match expected value of %lu\n", timestamp, EXTENDED_TIMESTAMP);
+		g_test_fail();
+		return;
+	}
+
+	if (extended_value_len != EXTENDED_VALUE_LEN) {
+		fclose(spool);
+		printf("Returned value of extended 'extended_value_len' %lu doesn't match expected value of %u\n", extended_value_len, EXTENDED_VALUE_LEN);
+		g_test_fail();
+		return;
+	}
+
+	extended_value = malloc(extended_value_len+1); /* Null-terminate it ourself. */
+	if (extended_value == NULL) {
+		fclose(spool);
+		printf("failed to malloc for readback of extended datapoint: %s\n", strerror(errno));
+		g_test_fail();
+		return;
+	}
+	ret = fread(extended_value, 1, extended_value_len, spool);
+	if (ret != extended_value_len) {
+		if (feof(spool) != 0) {
+			printf("Unexpected short read of %d bytes from spool while reading back extended data point, end of file\n", ret);
+		}
+		if (ferror(spool) != 0) {
+			printf("Unexpected short read of %d bytes from spool while reading back extended data point, there was an error\n", ret);
+		}
+		free(extended_value);
+		fclose(spool);
+		printf("Can't continue after short read\n");
+		g_test_fail();
+		return;
+	}
+	extended_value[extended_value_len] = '\0'; /* Guaranteed termination. */
+	ret = strncmp(EXTENDED_VALUE, extended_value, extended_value_len);
+	if (ret != 0) {
+		printf("Returned value of extended 'extended_value' %s doesn't match expected value of %s\n", extended_value, EXTENDED_VALUE);
+		g_test_fail();
+		/* Don't need to return, fall through to regular cleanup. */
+	}
+
 	// Cleanup and finish
-	free(written_spool_path);
+	free(extended_value);
 	ret = fclose(spool);
 	if (ret != 0) {
 		printf("failed to close spool file for reread as final action, meh: %s\n", strerror(errno));
