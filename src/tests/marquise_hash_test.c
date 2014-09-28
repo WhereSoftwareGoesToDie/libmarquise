72abb169d78b014b41c2471936f7891832c38fdb
Sharif Olorin <sio@tesser.org>
2014-09-28 13:08:37 +1000

    Add test for LSB-clearing in marquise_hash_identifier

--- src/tests/marquise_hash_test.c
+++ src/tests/marquise_hash_test.c
@@ -11,8 +11,16 @@ void test_hash_identifier() {
 	g_assert_cmpint(address, ==, 7602883380529707052);
 }
 
+void test_hash_clear_lsb() {
+	const char *id = "bytes:tx,collection_point:syd1,ip:110.173.152.33,";
+	size_t id_len = strlen(id);
+	uint64_t address = marquise_hash_identifier((const unsigned char*) id, id_len);
+	g_assert_cmpint(address, ==, -8873247187777138600);
+}
+
 int main(int argc, char **argv) {
 	g_test_init(&argc, &argv, NULL);
 	g_test_add_func("/marquise_hash_identifier/hash", test_hash_identifier);
+	g_test_add_func("/marquise_hash_identifier/clear_lsb", test_hash_clear_lsb);
 	return g_test_run();
 }
